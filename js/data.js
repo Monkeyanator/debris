ed2c0f1a5f5381cdb4ab8d8570ae25f8e2df8d44
Sam Naser <sammnaser@gmail.com>
2017-11-13 01:02:26 -0600

    Thicc commit

---
+++ js/data.js
@@ -0,0 +1,6 @@
+var markers = [
+    {'description': 'Stop sign', 'markerUrl': 'https://c1.staticflickr.com/4/3052/2673179714_c0dd1346ff_b.jpg', 'location': {'lat': 38.164871, 'lng': -85.585417}},
+    {'description': 'Issues', 'markerUrl': 'http://www.buzzhunt.co.uk/wp-content/2013/07/Tree-in-the-way.jpg', 'location': {'lat': 38.185, 'lng': -85.585417}},
+    {'description': 'House torn down', 'markerUrl': 'http://img.timeinc.net/time/photoessays/2011/japan_reacts/japan_reacts_01.jpg', 'location': {'lat': 38.164, 'lng': -85.63}},
+    {'description': 'Tree down', 'markerUrl': 'http://probalrashid.com/wp-content/uploads/2015/05/IMG_7184.jpg', 'location': {'lat': 38.1948, 'lng': -85.555417}}
+];
---
+++ js/map.js
@@ -0,0 +1,38 @@
+function initMap() {
+    var initialLocation = {lat: 38.164871, lng: -85.58541799999999};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: initialLocation
+    });
+
+   //use locations from data.js for the markers
+   //data generated from project.py script
+   for( i = 0; i < markers.length; i++) {
+    
+    //load position from data.js
+    var position = new google.maps.LatLng(markers[i]['location']['lat'], markers[i]['location']['lng']);
+
+    //generate content string based on url
+    console.log(markers[i].markerUrl);
+    var contentString = "<div><img width='180px' src="
+        + "'" + markers[i].markerUrl + "''"
+        + "/></div>"
+        + "<a style='width: 180px;' class='waves-effect waves-light btn modal-trigger' href='#modal2'>Resolve</a>";
+
+    marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: markers[i]['description'],
+    });
+
+    marker.content = contentString;
+    var infoWindow = new google.maps.InfoWindow();    
+
+    marker.addListener('click', function(){
+        infoWindow.setContent(this.content);
+        infoWindow.open(this.getMap(), this);
+    });
+
+  }
+
+}
