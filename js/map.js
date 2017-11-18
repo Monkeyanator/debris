ef9b7837d1afc4aa0153100907e750f83f35a87b
Michael Guo <McGuo@Michaels-MacBook-Pro-3.local>
2017-11-18 00:01:18 -0600

    updated indenting

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
 function initMap() {
-    var initialLocation = {lat: 38.164871, lng: -85.58541799999999};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: initialLocation
-    });
+  var initialLocation = {lat: 38.164871, lng: -85.58541799999999};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: initialLocation
+  });
 
-   //use locations from data.js for the markers
-   //data generated from project.py script
-   for( i = 0; i < markers.length; i++) {
+  //use locations from data.js for the markers
+  //data generated from project.py script
+  for( i = 0; i < markers.length; i++) {
 
     //load position from data.js
     var position = new google.maps.LatLng(markers[i]['location']['lat'], markers[i]['location']['lng']);
@@ -15,20 +15,20 @@ function initMap() {
     //generate content string based on url
     console.log(markers[i].markerUrl);
     var contentString = "<div><img width='180px' src="
-        + "'" + markers[i].markerUrl + "''"
-        + "/></div>"
-        + "<a style='width: 180px;' class='waves-effect waves-light btn modal-trigger' href='#modal2'>Resolve</a>";
+    + "'" + markers[i].markerUrl + "''"
+    + "/></div>"
+    + "<a style='width: 180px;' class='waves-effect waves-light btn modal-trigger' href='#modal2'>Resolve</a>";
 
     marker = new google.maps.Marker({
-        position: position,
-        map: map,
-        title: markers[i]['description'],
+      position: position,
+      map: map,
+      title: markers[i]['description'],
     });
     marker.content = contentString;
     var infoWindow = new google.maps.InfoWindow();
     marker.addListener('click', function(){
-        infoWindow.setContent(this.content);
-        infoWindow.open(this.getMap(), this);
+      infoWindow.setContent(this.content);
+      infoWindow.open(this.getMap(), this);
     });
 
   }
@@ -39,7 +39,7 @@ function initMap() {
     var position = new google.maps.LatLng(latitude, longitude);
     addMarker(position);
   });
-// 38.185, -85.591749
+  // 38.185, -85.591749
   function addMarker(location) {
     var icon = new google.maps.Marker({
       position: location,
