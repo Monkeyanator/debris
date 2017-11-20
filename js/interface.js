a6970cf2c2bd3ef2551b214f53290ed99902c6a7
Michael Guo <McGuo@Michaels-MacBook-Pro-3.local>
2017-11-19 22:37:15 -0600

    blah

--- js/interface.js
+++ js/interface.js
@@ -10,11 +10,6 @@ $(document).ready(function(){
     }
   });
 
-
-  // $('.drop1').css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0}, 200);
-
-
-
 // if we cancel the btn, we reset all values to default and we hide
   $("#form-btn-cancel").click(() => {
     $("#pac-card").hide();
@@ -27,7 +22,7 @@ $(document).ready(function(){
     geoLocate();
   });
 
-  
+
   //set up events for resolving debris submission
   $("#resolve-debris-submit").click(() => {
 
