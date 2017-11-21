bfb70abd91a33e30450f1da4a3d9b4b3ec987f10
Michael Guo <McGuo@Michaels-MacBook-Pro-3.local>
2017-11-20 21:30:28 -0600

    added data

--- js/data.js
+++ js/data.js
@@ -1,8 +1,13 @@
 var markers = [
-    {'title': 'Stop sign', 'description':'We noticed a stop sign that fell over on the intersection of Harrison and Fairground','markerUrl': 'https://c1.staticflickr.com/4/3052/2673179714_c0dd1346ff_b.jpg', 'location': {'lat': 38.164871, 'lng': -85.585417}},
-    {'title': 'Tree on roads', 'description':'There is a tree in the middle of Watterson trail!!','markerUrl': 'http://www.buzzhunt.co.uk/wp-content/2013/07/Tree-in-the-way.jpg', 'location': {'lat': 38.185, 'lng': -85.585417}},
+    {'title': 'Stop sign', 'description':'We noticed a stop sign that fell over on the intersection of Harrison and Fairground.','markerUrl': 'https://c1.staticflickr.com/4/3052/2673179714_c0dd1346ff_b.jpg', 'location': {'lat': 38.164871, 'lng': -85.585417}},
+    {'title': 'Tree on the road', 'description':'There is a tree in the middle of Watterson trail!!','markerUrl': 'http://www.buzzhunt.co.uk/wp-content/2013/07/Tree-in-the-way.jpg', 'location': {'lat': 38.185, 'lng': -85.585417}},
     {'title': 'House torn down', 'description':'We found a tree torn down at the end of Fern Creek!','markerUrl': 'http://img.timeinc.net/time/photoessays/2011/japan_reacts/japan_reacts_01.jpg', 'location': {'lat': 38.164, 'lng': -85.63}},
-    {'title': 'Homes destroyed', 'description': 'Destroyed homes near Penion Dr','markerUrl': 'http://probalrashid.com/wp-content/uploads/2015/05/IMG_7184.jpg', 'location': {'lat': 38.1948, 'lng': -85.555417}},
-    {'title': 'Flipped Car', 'description': 'Car flipped over in downtown Louisville','markerUrl': 'https://www.fema.gov/media-library-data/ff26c357-9e74-4459-a4ba-1a98dc955284/66035.jpg', 'location': {'lat': 38.256433, 'lng': -85.755246}},
+    {'title': 'Homes destroyed', 'description': 'Destroyed homes near Penion Dr.','markerUrl': 'http://probalrashid.com/wp-content/uploads/2015/05/IMG_7184.jpg', 'location': {'lat': 38.1948, 'lng': -85.555417}},
+    {'title': 'Flipped Car', 'description': 'Car flipped over in downtown Louisville.','markerUrl': 'https://www.fema.gov/media-library-data/ff26c357-9e74-4459-a4ba-1a98dc955284/66035.jpg', 'location': {'lat': 38.256433, 'lng': -85.755246}},
+    {'title': 'Flooded River', 'description': 'The creek is flooding along E Liberty St. Causing traffic delays!','markerUrl': 'http://all-geo.org/highlyallochthonous/wp-content/uploads/2011/01/Marsh-hood-flood.jpg', 'location': {'lat': 38.249627, 'lng': -85.730885}},
+    {'title': 'Shattered Windows', 'description': 'Our neighbours have shattered windows and the glass is a health hazard to kids.','markerUrl': 'https://static1.squarespace.com/static/58a33fd9725e252227fdb4ce/592347d7f76cad8d402547af/592347e9f76cad8d4025f707/1495484393834/Broken-window.jpg?format=original', 'location': {'lat': 38.198811, 'lng': -85.659849}},  
+    {'title': 'Flipped Car', 'description': 'Car flipped over in downtown Louisville.','markerUrl': 'https://www.fema.gov/media-library-data/ff26c357-9e74-4459-a4ba-1a98dc955284/66035.jpg', 'location': {'lat': 38.256433, 'lng': -85.755246}},
+    {'title': 'Flipped Car', 'description': 'Car flipped over in downtown Louisville.','markerUrl': 'https://www.fema.gov/media-library-data/ff26c357-9e74-4459-a4ba-1a98dc955284/66035.jpg', 'location': {'lat': 38.256433, 'lng': -85.755246}},
+    {'title': 'Flipped Car', 'description': 'Car flipped over in downtown Louisville.','markerUrl': 'https://www.fema.gov/media-library-data/ff26c357-9e74-4459-a4ba-1a98dc955284/66035.jpg', 'location': {'lat': 38.256433, 'lng': -85.755246}},
 
 ];
